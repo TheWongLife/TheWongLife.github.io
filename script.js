1c1a6a12474baf8f372cef29bc779de268571e01
Nicholas Wong <nwong27@illinois.edu>
2024-05-29 16:56:53 -0500

    Updating Github Pages

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 var cursor = true;
 var speed = 250;
 setInterval(() => {
-  if(cursor) {
+  if (cursor) {
     document.getElementById('cursor').style.opacity = 0;
     cursor = false;
-  }else {
+  } else {
     document.getElementById('cursor').style.opacity = 1;
     cursor = true;
   }
