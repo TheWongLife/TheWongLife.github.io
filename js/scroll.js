7533697410a2fbebf53ad0bc226badc704550cf5
Nicholas Wong <nwong27@illinois.edu>
2025-02-16 18:45:21 -0600

    Updated Website

--- js/scroll.js
+++ js/scroll.js
@@ -1,9 +1,7 @@
 let mybutton = document.getElementById("btn-back-to-top");
-
 window.onscroll = function () {
   scrollFunction();
 };
-
 function scrollFunction() {
   if (
     document.body.scrollTop > 20 ||
@@ -14,9 +12,7 @@ function scrollFunction() {
     mybutton.style.display = "none";
   }
 }
-
 mybutton.addEventListener("click", backToTop);
-
 function backToTop() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
